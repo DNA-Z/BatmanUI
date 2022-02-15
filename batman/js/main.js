ab3aa0c3596566608fd8e50dae952769f7cc0140
Inna Borodina <47341403+DNA-Z@users.noreply.github.com>
2022-02-15 14:31:43 +0300

    Add main block

---
+++ batman/js/main.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper('.main-slider', {
+    slidesPerView: 2,
+    spaceBetween: 30,
+    loop: true,
+    navigation: {
+      nextEl: '.main-slader__arrow',
+    },
+  });
+  
